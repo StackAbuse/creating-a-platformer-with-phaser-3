a6c3f0832aaee077f6b053aed681d79656b2bf5a
Marcus Sanatan <msanatan@gmail.com>
2019-06-18 07:49:06 -0400

    Add assets for game

---
+++ assets/tilesets/kenney_simple_platformer.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="kenney_simple_platformer" tilewidth="64" tileheight="64" tilecount="98" columns="14">
+ <image source="platformPack_tilesheet.png" width="896" height="448"/>
+</tileset>
