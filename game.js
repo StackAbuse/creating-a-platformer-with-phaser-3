2f9e28173be788df6af601bdd5beee423470ccbe
Marcus Sanatan <msanatan@gmail.com>
2019-06-19 17:17:52 -0400

    Add player animations to the game
    Now it doesn't look weird when we run/jump around our level

--- game.js
+++ game.js
@@ -24,46 +24,121 @@ const config = {
 const game = new Phaser.Game(config);
 
 function preload() {
+  // Image layers from Tiled can't be exported to Phaser 3 (as yet)
+  // So we add the background image separately
   this.load.image('background', 'assets/images/backgroundEmpty.png');
-  this.load.image('spike', 'assets/images/spike.png');
+  // Load the tileset image file, needed for the map to know what
+  // tiles to draw on the screen
   this.load.image('tiles', 'assets/tilesets/platformPack_tilesheet.png');
+  // Even though we load the tilesheet with the spike image, we need to
+  // load the Spike image separately for Phaser 3 to render it
+  this.load.image('spike', 'assets/images/spike.png');
+  // Load the export Tiled JSON
   this.load.tilemapTiledJSON('map', 'assets/tilemaps/level1.json');
-  // Load player animations
-  this.load.atlas('player', 'assets/images/kenney_player.png', 'assets/images/kenney_player_atlas.json');
+  // Load player animations from the player spritesheet and atlas JSON
+  this.load.atlas('player', 'assets/images/kenney_player.png',
+    'assets/images/kenney_player_atlas.json');
 }
 
 function create() {
+  // Create a tile map, which is used to bring our level in Tiled
+  // to our game world in Phaser
   const map = this.make.tilemap({ key: 'map' });
+  // Add the tileset to the map so the images would load correctly in Phaser
   const tileset = map.addTilesetImage('kenney_simple_platformer', 'tiles');
-  const backgroundImage = this.add.image(0, 0, 'background').setOrigin(0, 0).setScale(2, 0.8);
+  // Place the background image in our game world
+  const backgroundImage = this.add.image(0, 0, 'background').setOrigin(0, 0)
+  // Scale the image to better match our game's resolution
+  backgroundImage.setScale(2, 0.8);
+  // Add the platform layer as a static group, the player would be able
+  // to jump on platforms like world collisions but they shouldn't move
   const platforms = map.createStaticLayer('Platforms', tileset, 0, 200);
+  // There are many ways to set collision between tiles and players
+  // As we want players to collide with all of the platforms, we tell Phaser to
+  // set collisions for every tile in our platform layer whose index isn't -1.
+  // Tiled indices can only be >= 0, therefore we are colliding with all of
+  // the platform layer
   platforms.setCollisionByExclusion(-1, true);
-  this.spikes = map.createFromObjects('Spikes', 71, {key: 'spike'}, this);
+  // Get the spikes from the object layer of our Tiled map. The 2nd argument of
+  // createFromObjects is the `gid` of the objects, a value given by Tiled. We
+  // can get the gid from the exported JSON
+  this.spikes = map.createFromObjects('Spikes', 71, { key: 'spike' }, this);
   // Move spikes down 200 pixel to match their original positions on the platforms
   this.spikes.forEach(element => {
     element.y += 200;
   });
 
+  // Add the player to the game world
   this.player = this.physics.add.sprite(50, 300, 'player');
   this.player.setBounce(0.1); // our player will bounce from items
   this.player.setCollideWorldBounds(true); // don't go out of the map
   this.physics.add.collider(this.player, platforms);
 
+  // Enable user input via cursor keys
   this.cursors = this.input.keyboard.createCursorKeys();
+
+  // Create the walking animation using the last 2 frames of
+  // the atlas' first row
+  this.anims.create({
+    key: 'walk',
+    frames: this.anims.generateFrameNames('player', {
+      prefix: 'robo_player_',
+      start: 2,
+      end: 3,
+    }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  // Create an idle animation i.e the first frame
+  this.anims.create({
+    key: 'idle',
+    frames: [{ key: 'player', frame: 'robo_player_0' }],
+    frameRate: 10,
+  });
+
+  // Use the second frame of the atlas for jumping
+  this.anims.create({
+    key: 'jump',
+    frames: [{ key: 'player', frame: 'robo_player_1' }],
+    frameRate: 10,
+  });
 }
 
 function update() {
-  // Moving left and right
-  this.player.setVelocityX(0);
+  // Control the player with left or right keys
   if (this.cursors.left.isDown) {
-    this.player.body.setVelocityX(-200);
+    this.player.setVelocityX(-200);
+    if (this.player.body.onFloor()) {
+      this.player.play('walk', true);
+    }
   } else if (this.cursors.right.isDown) {
-    this.player.body.setVelocityX(200);
+    this.player.setVelocityX(200);
+    if (this.player.body.onFloor()) {
+      this.player.play('walk', true);
+    }
+  } else {
+    // If no keys are pressed, the player keeps still
+    this.player.setVelocityX(0);
+    // Only show the idle animation if the player is footed
+    // If this is not included, the player would look idle while jumping
+    if (this.player.body.onFloor()) {
+      this.player.play('idle', true);
+    }
   }
 
-  // Jumping
-  if ((this.cursors.space.isDown) && this.player.body.onFloor()) {
-    this.player.body.setVelocityY(-350);
+  // Player can jump while walking any direction by pressing the space bar
+  // or the 'UP' arrow
+  if ((this.cursors.space.isDown || this.cursors.up.isDown) && this.player.body.onFloor()) {
+    this.player.setVelocityY(-350);
+    this.player.play('jump', true);
+  }
+
+  // If the player is moving to the right, keep them facing forward
+  if (this.player.body.velocity.x > 0) {
+    this.player.setFlipX(false);
+  } else if (this.player.body.velocity.x < 0) {
+    // otherwise, make them face the other side
+    this.player.setFlipX(true);
   }
 }
-
